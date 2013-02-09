6feefe12383309e763dd0c7e2c1fbe7047c926bd
Tom Peters <tom@tjpeters.net>
2013-02-10 08:41:32 +1000

    Added Desktop object and functionality for getting screen info

---
+++ Paneless/Core.UnitTests/DesktopTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using EasyAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.UnitTests
+{
+    [TestClass]
+    public class DesktopTests
+    {
+        [TestMethod]
+        public void TestPopulateScrens()
+        {
+            Desktop sut = new Desktop();
+            sut.PopulateScreens();
+            sut.Screens.ShouldNotBeNull().And.Count().ShouldBeGreaterThan(0); // Environment dependent (can't be headless)
+        }
+    }
+}
---
+++ Paneless/Core/Desktop.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace Core
+{
+    public class Desktop
+    {
+        private List<Screen> _screens;
+
+        public void PopulateScreens()
+        {
+            _screens = Screen.AllScreens.ToList();
+        }
+
+        public List<Screen> Screens
+        {
+            get { return _screens; }
+        }
+    }
+}
