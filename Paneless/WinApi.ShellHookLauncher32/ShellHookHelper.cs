4c46db2823fd0cec87a979854be7be05aea1f6d2
Tom Peters <tom@tjpeters.net>
2013-04-13 08:36:32 +1000

    ShellHookLaunchers made very DRY - removed duplicate dll calls

--- Paneless/WinApi.ShellHookLauncher32/ShellHookHelper.cs
+++ Paneless/WinApi.ShellHookLauncher32/ShellHookHelper.cs
@@ -1,19 +1,7 @@
-﻿using System.Runtime.InteropServices;
-
-namespace WinApi.ShellHookLauncher64
+﻿namespace WinApi.ShellHookLauncher64
 {
-    internal static class ShellHookHelper
+    internal static partial class ShellHookHelper
     {
         public const string DllFileName = "WinApi.ShellHook32.dll";
-
-        #region HookSetupMethods
-
-        [DllImport(DllFileName, CallingConvention = CallingConvention.Cdecl)]
-        public static extern bool SetupWndProcHook();
-
-        [DllImport(DllFileName, CallingConvention = CallingConvention.Cdecl)]
-        public static extern bool SetupGetMsgHook();
-
-        #endregion
     }
 }
---
+++ Paneless/WinApi.ShellHookLauncher64/ShellHookHelperDllCalls.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace WinApi.ShellHookLauncher64
+{
+    internal static partial class ShellHookHelper
+    {
+        [DllImport(DllFileName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool SetupWndProcHook();
+
+        [DllImport(DllFileName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool SetupGetMsgHook();
+    }
+}
